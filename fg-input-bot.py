a4078402158a4c838997ca87a1b4b0540b177010
Michael <mslu5805@gmail.com>
2022-03-26 05:15:22 -0400

    Parses through glossary.json, functions but doesn't look nice atm

--- fg-input-bot.py
+++ fg-input-bot.py
@@ -4,10 +4,14 @@
 # bot.py
 import os
 from infil_scraper import infilScraper
-
 import discord
+import json
+
 from dotenv import load_dotenv
 from discord.ext import commands
+
+scrape_mode = False
+debug = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,18 +21,25 @@ GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 bot =  commands.Bot(command_prefix='!')
 
+# Open and store json file data
+
+with open("glossary.json", encoding='utf-8') as jsonFile:
+    glossary = json.load(jsonFile)
+    jsonFile.close()
+
+
 @client.event
 async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-    
-# https://glossary.infil.net/?t=
+    if debug:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+        
+# All glossary items obtained from https://glossary.infil.net/json/glossary.json once a day
 
 @client.event
 async def on_message(message):
@@ -36,8 +47,25 @@ async def on_message(message):
         return
     scraper = infilScraper()
     if '!glossary' in message.content:
-        key_words, search_words = scraper.key_words_search_words(message.content)
-        result_text = scraper.search(key_words)
+        # key_words = scraper.key_words_search_words(message.content)
+        search_words = message.content[10::]
+
+        if scrape_mode:
+            result_text = scraper.search(message.content)
+        else:
+            term = list(filter(lambda item: item['term'].lower() == search_words.lower(), glossary))
+            #print(term)
+            if not term:
+                for altterm in glossary:
+                    if "altterm" in altterm:
+                        if search_words.lower() in (alt.lower() for alt in altterm["altterm"]):                    
+                            term = altterm
+            if debug:
+                print(message.content)
+                print(search_words)
+            #TODO: make this readable
+            result_text = term
+
 
 
     if len(result_text) > 0:
