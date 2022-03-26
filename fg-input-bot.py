5e3d411c3e7ed6511b31a783ba92f69d26e2834a
Michael <mslu5805@gmail.com>
2022-03-26 18:59:44 -0400

    Added discord embed, needs more cleanup, video embed doesn't work

--- fg-input-bot.py
+++ fg-input-bot.py
@@ -3,15 +3,18 @@
 
 # bot.py
 import os
-from infil_scraper import infilScraper
+from unicodedata import name
 import discord
 import json
+import re
 
 from dotenv import load_dotenv
 from discord.ext import commands
 
 scrape_mode = False
 debug = True
+if scrape_mode:
+    from infil_scraper import infilScraper
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -40,37 +43,97 @@ async def on_ready():
         )
         
 # All glossary items obtained from https://glossary.infil.net/json/glossary.json once a day
+# might move this to it's own .py cuz it's getting kinda large :sweat:
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    scraper = infilScraper()
     if '!glossary' in message.content:
         # key_words = scraper.key_words_search_words(message.content)
         search_words = message.content[10::]
 
         if scrape_mode:
+            scraper = infilScraper()
             result_text = scraper.search(message.content)
         else:
+            result_found = False
             term = list(filter(lambda item: item['term'].lower() == search_words.lower(), glossary))
             #print(term)
-            if not term:
-                for altterm in glossary:
-                    if "altterm" in altterm:
-                        if search_words.lower() in (alt.lower() for alt in altterm["altterm"]):                    
-                            term = altterm
-            if debug:
-                print(message.content)
-                print(search_words)
-            #TODO: make this readable
-            result_text = term
+            if term:
+                term = term[0]
+                result_found = True
+            elif not term:
+                for dict in glossary:
+                    if "altterm" in dict:
+                        if search_words.lower() in (alt.lower() for alt in dict["altterm"]):                    
+                            term = dict
+                            result_found = True
 
+            # really bad way of checking if dict has these keys
+            if result_found:
+                hasAlt = "altterm" in term
+                hasVideo = "video" in term
+                hasImage = "image" in term
+                hasJP = "jp" in term
+                hasGameSpecific = "games" in term
 
+                if debug:   
+                    print(message.content)
+                    print(search_words)
+                    print("type: ", type(term))
+                    print(hasAlt)
+                    print(hasVideo)
+                    print(hasImage)
+                    print(hasJP)
+                    print(hasGameSpecific)
+                #TODO: make this readable
+                
+                embed_kwargs = {}
 
-    if len(result_text) > 0:
-        await message.channel.send(result_text)
-    else:
-        await message.channel.send("no results found :(")
+                
+
+                # replace embedded with underline to indicate more terms
+                #result_text = ""
+                #result_text += (term["term"] + "\n")
+                if(hasImage):
+                    embed = discord.Embed(title=term["term"], color=0x00ff00)
+                    embed.set_image(url=("https://glossary.infil.net/images/terms/" + term["term"].replace(" ", "%20") + "." + term["image"][0]))
+                    embed.set_footer(text=term["image"][1])
+                    #result_text += ("https://glossary.infil.net/images/terms/" + term["term"].replace(" ", "%20") + "." + term["image"][0])
+                    #footer_text = term["image"][1]         
+                elif(hasVideo):
+                   # embed = discord.Embed(
+                   #     title=embed.title, 
+                   #     fields={
+                   #         for field in embed.fields:
+                   #             name=field.name
+                   #             }
+                   #          )
+                    embed = discord.Embed(title=term["term"], video=("https://gfycat.com/" + term["video"][0]), color=0x00ff00)
+                    embed.add_field(name="video link:", value=("https://gfycat.com/" + term["video"][0]))
+                    #embed.set_image(url=("https://gfycat.com/" + term["video"][0]))
+                    embed.set_footer(text=term["video"][1])
+                    #result_text += ("https://gfycat.com/" + term["video"][0])
+                    #footer_text = term["video"][1]
+                else:
+                    embed = discord.Embed(title=term["term"], color=0x00ff00)
+                if(hasAlt):
+                    embed.add_field(name="Alternate Name(s): ", value=", ".join(str(alt) for alt in term["altterm"]))
+                    #result_text += ", ".join(str(alt) for alt in term["altterm"]) + "\n"
+                if(hasJP):
+                    embed.add_field(name="JP name(s): ", value=term["jp"])
+                    #result_text += (term["jp"] + "\n")
+                #result_text += ("\n" + term["def"] + "\n")
+                embed.description = term["def"]
+                if(hasGameSpecific):
+                    embed.add_field(name="Specific to: ", value=", ".join(str(alt) for alt in term["games"]))
+                    #result_text += ("This game is specific to: \n")
+                    #result_text += ", ".join(str(game) for game in term["games"]) + "\n"
+
+                # send the embedded message
+                await message.channel.send(embed=embed)
+            else: 
+                await message.channel.send("No results found")
 
 client.run(TOKEN)
